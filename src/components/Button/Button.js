eda2ff23ce3bee0a8ab0857424c1966fce9c09b6
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-10-23 15:13:24 +0100

    Button accepts category, size and text - soon to be props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,10 +4,13 @@ import styles from "./Button.module.css";
 const Button = () => {
 	const buttonText = "This is a button";
 	const buttonCategory = "primary";
+	const buttonSize = "medium";
 	let button;
-	if (buttonCategory) {
+	if (buttonCategory && buttonSize) {
 		button = (
-			<button className={`${styles.button} ${styles[buttonCategory]}`}>
+			<button
+				className={`${styles.button} ${styles[buttonCategory]} ${styles[buttonSize]}`}
+			>
 				{buttonText}
 			</button>
 		);

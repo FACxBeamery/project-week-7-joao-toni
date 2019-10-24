1a1c8d555563b9e8862fdef696dd9516d47ba9a0
Antonio Gargaro <antoniogargaro97@gmail.com>
2019-10-24 15:15:34 +0100

    task cards change on progression updates

--- src/components/TaskCard/TaskCard.js
+++ src/components/TaskCard/TaskCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./TaskCard.module.css";
 
 import Button from "../Button";
@@ -6,6 +6,23 @@ import Badge from "../Badge";
 import ProfileArea from "../ProfileArea";
 
 const TaskCard = ({ id, title, description, time, taskWith }) => {
+	// Consts
+	const [typeProg, typeComp] = ["inprogress", "complete"];
+
+	const [type, setType] = useState(typeProg);
+
+	// handleClicks
+	const typeProgHandleClick = () => setType(typeComp);
+	const typeCompHandleClick = () => setType(typeProg);
+
+	// Conditionals
+	const typeIsProg = type === typeProg;
+	const typeIsComp = type === typeComp;
+
+	const btnText = typeIsProg ? "Complete" : "Re-open task";
+	const btnCategory = typeIsProg ? "primary" : "outlined";
+	const handleClick = typeIsProg ? typeProgHandleClick : typeCompHandleClick;
+
 	const Header = () => <h1>{title}</h1>;
 	const SubHeader = () => <p className={styles.subHeader}>Due by: {time}</p>;
 
@@ -20,11 +37,16 @@ const TaskCard = ({ id, title, description, time, taskWith }) => {
 					<p>{description}</p>
 				</div>
 				<div className={styles.buttonContainer}>
-					<Button size={"medium"} buttonText={"Complete"} />
+					<Button
+						size={"medium"}
+						buttonText={btnText}
+						onClick={handleClick}
+						category={btnCategory}
+					/>
 				</div>
 			</div>
 			<div className={styles.rightContent}>
-				<Badge />
+				<Badge type={type} />
 				<ProfileArea {...taskWith} />
 			</div>
 		</div>

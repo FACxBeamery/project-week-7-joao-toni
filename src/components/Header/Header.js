2e094a660277f258c839f5b72fe3c6eef716b831
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-10-25 12:05:55 +0100

    Header accepts extra prop if highlighted

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,30 @@
 import React from "react";
 import styles from "./Header.module.css";
 
-const Header = ({ size = "medium", text, sans = false }) => {
+const Header = ({
+	size = "medium",
+	text,
+	sans = false,
+	highlighted = false
+}) => {
 	let header;
 
 	if (size === "large") {
-		header = <h1 className={`${styles.header} ${styles.large}`}>{text}</h1>;
+		header = (
+			<h1
+				className={`${styles.header} ${styles.large} ${
+					highlighted ? styles[text] : ""
+				}`}
+			>
+				{text}
+			</h1>
+		);
 	} else if (size === "medium") {
 		header = (
 			<h2
 				className={`${styles.header} ${styles.medium} ${
 					sans ? styles.sans : ""
-				}`}
+				} ${highlighted ? styles[text] : ""}`}
 			>
 				{text}
 			</h2>

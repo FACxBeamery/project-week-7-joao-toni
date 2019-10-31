037411a528dc83bc692e46fa0f1a4eb317f307f8
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-10-31 15:14:09 +0000

    tests cleanup

--- src/components/TaskOverviewList/TaskOverviewRow/TaskOverviewRow.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import TaskOverviewRow from "./TaskOverviewRow";
-
-describe("Testing the TaskOverviewRow component", () => {
-	test("TaskOverviewRow renders ", () => {
-		render(<TaskOverviewRow />);
-	});
-});

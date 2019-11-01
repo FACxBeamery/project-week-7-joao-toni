62223da4ff36c9ef083948e0900038b630936042
Antonio Gargaro <antoniogargaro97@gmail.com>
2019-11-01 12:49:59 +0000

    Filter complete tasks from list

--- src/components/TaskOverviewList/TaskOverviewList.test.js
+++ src/components/TaskOverviewList/TaskOverviewList.test.js
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import TaskOverviewList, { getDayName } from "./TaskOverviewList";
-import { tasksData } from "../../dummyData/tasksData";
+import { dummyData } from "../../dummyData/tasksData";
 
 afterEach(cleanup);
+
+const tasksData = {
+	Monday: [
+		{
+			id: 1,
+			title: "Intro Task",
+			description: "Meet the team!",
+			time: "12:00",
+			taskWith: {
+				name: "Yob Yan",
+				position: "Grad Manager"
+			},
+			progress: "inprogress"
+		},
+		{
+			id: 2,
+			title: "Meet Your Laptop!",
+			description: "Unbox yo goodies",
+			time: "17:00",
+			taskWith: {
+				name: "Grads",
+				position: "Grad Team"
+			},
+			progress: "inprogress"
+		}
+	],
+	Tuesday: [
+		{
+			id: 3,
+			title: "Intro Task",
+			description: "Meet the team!",
+			time: "12:00",
+			taskWith: {
+				name: "Yob Yan",
+				position: "Grad Manager"
+			},
+			progress: "complete"
+		},
+		{
+			id: 9,
+			title: "Intro Task3",
+			description: "Meet the team!",
+			time: "12:00",
+			taskWith: {
+				name: "Yob Yan",
+				position: "Grad Manager"
+			},
+			progress: "inprogress"
+		}
+	],
+	Wednesday: [
+		{
+			id: 4,
+			title: "Beamery Product intro meeting",
+			description: "Meet the product!",
+			time: "12:00",
+			taskWith: {
+				name: "Yob Yan",
+				position: "Grad Manager"
+			},
+			progress: "inprogress"
+		}
+	],
+	Thursday: [],
+	Friday: []
+};
 
 describe("Testing the TaskOverviewList component", () => {
 	test("Current day index is mapped to week day string", () => {
@@ -20,7 +86,10 @@ describe("Testing the TaskOverviewList component", () => {
 		);
 
 		const { getByText, getAllByText } = render(
-			<TaskOverviewList tasksData={tasksData} />
+			<TaskOverviewList
+				tasksData={tasksData}
+				setTasksData={(input) => input}
+			/>
 		);
 
 		const taskListHeader = getByText("Today's Overview");

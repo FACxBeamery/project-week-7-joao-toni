1e3e5057387d1770e48768c1759c854b0a42a33a
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-01 10:17:51 +0000

    tasksData - more dataaaa

--- src/dummyData/tasksData.js
+++ src/dummyData/tasksData.js
@@ -50,7 +50,41 @@ const tasksData = {
 		}
 	],
 	Thursday: [],
-	Friday: []
+	Friday: [
+		{
+			id: 5,
+			title: "Beamery Product intro meeting",
+			description: "Meet the product!",
+			time: "09:00",
+			taskWith: {
+				name: "Yob Yan",
+				position: "Grad Manager"
+			},
+			progress: "inprogress"
+		},
+		{
+			id: 6,
+			title: "Beamery Product intro meeting",
+			description: "Meet the product!",
+			time: "11:00",
+			taskWith: {
+				name: "Yob Yan",
+				position: "Grad Manager"
+			},
+			progress: "inprogress"
+		},
+		{
+			id: 7,
+			title: "Beamery Product intro meeting",
+			description: "Meet the product!",
+			time: "12:00",
+			taskWith: {
+				name: "Yob Yan",
+				position: "Grad Manager"
+			},
+			progress: "inprogress"
+		}
+	]
 };
 
 export { tasksData };

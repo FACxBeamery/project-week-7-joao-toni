d9baa6bc6eedc09798196fb404ef3c2a47d47e6c
“joaoviana” <joaopadraoviana@gmail.com>
2019-11-03 13:50:52 +0000

    taskcard testing - removed unecessary import

--- src/components/TaskCard/TaskCard.test.js
+++ src/components/TaskCard/TaskCard.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import TaskCard from "./TaskCard";
-import { dummyData } from "../../dummyData/tasksData";
 
 afterEach(cleanup);
 

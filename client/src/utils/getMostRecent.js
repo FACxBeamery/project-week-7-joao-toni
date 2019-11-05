a69bcf21ae87e3e5a0a07ee40653947265245e26
“joaoviana” <joaopadraoviana@gmail.com>
2019-11-05 18:34:51 +0000

    updated file structure

---
+++ client/src/utils/getMostRecent.js
@@ -0,0 +1,27 @@
+import { tasksData } from "../dummyData/tasksData";
+import { getDayName } from "./getDayName";
+
+export const getMostRecent = () => {
+    return new Promise((resolve, reject) => {
+        const now = new Date();
+        const currentDayOfTheWeek = getDayName(now.getDay());
+        const currentTime = `${now.getHours()}:${now.getMinutes()}`;
+        const currentTimeInSeconds = currentTime
+            .split(":")
+            .reduce((acc, time) => 60 * acc + time);
+        const tasksAvailable = tasksData[currentDayOfTheWeek];
+        if (tasksAvailable.length > 0) {
+            resolve(
+                tasksAvailable.find(
+                    task =>
+                        task.time
+                            .split(":")
+                            .reduce((acc, time) => 60 * acc + time) >
+                        currentTimeInSeconds
+                )
+            );
+        } else {
+            reject(new Error("No tasks available"));
+        }
+    });
+};

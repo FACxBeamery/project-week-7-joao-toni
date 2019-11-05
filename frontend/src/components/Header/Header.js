418f53b2fe4f1cb201e90964c788ed0e549f4a83
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-05 10:00:07 +0000

    extra comment for clearer explanation

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -13,6 +13,8 @@ const Header = ({
 
 	const isCentered = `${centered && styles.centered}`;
 	const isSans = `${sans && styles.sans}`;
+	// if highlighted is true, it is a header that represents a day of the week
+	// the header'stext can be Monday, Tuesday, Wednesday, ...
 	const isHighlighted = `${highlighted && styles[text.toLowerCase()]}`;
 
 	return (

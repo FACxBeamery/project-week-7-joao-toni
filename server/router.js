c9620a52f70ce18fbabe296c5125992f3e1c9d13
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-07 10:58:47 +0000

    POST /tasks with custom schema validator middleware

--- server/router.js
+++ server/router.js
@@ -1,8 +1,14 @@
 const express = require("express");
+
 const getTasks = require("./handlers/getTasks.js");
+const createTask = require("./handlers/createTask.js");
+
+const taskValidator = require("./middleware/taskValidator");
 
 const router = express();
 
 router.get("/tasks", getTasks);
+
+router.post("/tasks", taskValidator(true), createTask);
 
 module.exports = router;

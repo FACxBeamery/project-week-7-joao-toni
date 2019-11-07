46908ad7da9b6d7a9e66f1b506b958dd63da03b6
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-07 10:59:02 +0000

    createTask handler works

--- server/handlers/createTask.js
+++ server/handlers/createTask.js
@@ -1,16 +1,16 @@
-
+const addTask = require("../database/queries/addTask");
+const getDB = require("../database/dbConnection.js").getDb;
 
 const createTask = async (req, res) => {
+    const db = getDB();
     
-    try {
-        const {data} = await addTask(db.collection("tasks"), req.body);
-        res.status(201).json(data);
+    try {        
+        await addTask(db.collection("tasks"), req.body);
+        res.status(201).end();
 
     } catch(err) {
         res.status(404).json(err.message);
     }
 };
 
-
-// in router
-// app.get("/users", taskValidator, createTask); 
+module.exports = createTask;

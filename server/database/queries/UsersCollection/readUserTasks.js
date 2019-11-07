32b5d97151ec14efeaa951a64e18e2ada06472b7
Antonio Gargaro <antoniogargaro97@gmail.com>
2019-11-07 15:41:53 +0000

    Details for user tasks endpoint

--- server/database/queries/UsersCollection/readUserTasks.js
+++ server/database/queries/UsersCollection/readUserTasks.js
@@ -2,39 +2,35 @@ const ObjectId = require("mongodb").ObjectID;
 
 const readTask = require("../readTask");
 
-const readUserTasks = (users, findByUserId, tasks) => {
+const readUserTasks = (users, byUserIdQuery, tasks) => {
     return new Promise((resolve, reject) => {
-        users.findOne(findByUserId, async (err, result) => {
+        users.findOne(byUserIdQuery, async (err, result) => {
             if (err) reject(err);
 
-            let days = Object.keys(result.tasks);
+            let tasksGroupedByDaysOfTheWeek = result.tasks;
+            let daysOfTheWeekKeys = Object.keys(result.tasks);
 
-            let updatedTasks = await days.reduce(
-                async (previousBatch, currentDay, index) => {
-                    previousBatch = await previousBatch;
-                    console.log();
-
-                    console.log(`Processing batch ${index}...`);
-
-                    const currentDayPromises = result.tasks[currentDay]
-                        .map(task => readTask(tasks, { _id: new ObjectId(task.taskId) })
-                            .then((taskResult) => {
-                                let updatedUserTask = { ...taskResult[0], ...task };
-                                return updatedUserTask;
-                            })
-                            .catch((err) => console.error(err)));
-
+            let detailedUserTasks = await daysOfTheWeekKeys.reduce(
+                async (previousDayTasks, currentDay) => {
+                    previousDayTasks = await previousDayTasks;
+                    const currentDayPromises = tasksGroupedByDaysOfTheWeek[currentDay]
+                        .map(async taskOfCurrentDay => {
+                            try {
+                                let retrievedTaskDetails = await readTask(tasks, { _id: new ObjectId(taskOfCurrentDay.taskId) });
+                                let updatedTaskDetails = { ...retrievedTaskDetails[0], ...taskOfCurrentDay };
+                                return updatedTaskDetails;
+                            } catch (error) {
+                                console.log(error);
+                            }
+                        });
                     const promisesResult = await Promise.all(currentDayPromises);
-                    console.log(promisesResult);
-                    return { ...previousBatch, [currentDay]: promisesResult };
+                    return { ...previousDayTasks, [currentDay]: promisesResult };
                 }, Promise.resolve());
 
-            console.log(updatedTasks);
-
-            resolve(updatedTasks);
-
+            resolve(detailedUserTasks);
         });
     });
 };
+
 
 module.exports = readUserTasks;

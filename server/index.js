2e019f20cd7f2b68bc9a1b1fcd9247aacfe87396
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-08 14:12:54 +0000

    index.js server cleanup & destructuring of db functions

--- server/index.js
+++ server/index.js
@@ -1,18 +1,12 @@
 const express = require("express");
-// const formidable = require("express-formidable");
 const router = require("./router.js");
-const initDb = require("./database/dbConnection").initDb;
-const getDb = require("./database/dbConnection").getDb;
+const {initDb, getDb} = require("./database/dbConnection");
 const { addSeedData } = require("./testData/seed.js");
-
-// console.log(seed);
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(router);
-router.get("/tasks", () => console.log("r"));
-
 
 initDb()
     .then(() => {
@@ -22,9 +16,10 @@ initDb()
         // TODO Only for development
         addSeedData(db);
 
-
         app.listen(port, () => {
             console.log(`Server listening on port ${port}. Ready to accept requests!`);
         });
     })
     .catch(console.error);
+
+module.exports = app;

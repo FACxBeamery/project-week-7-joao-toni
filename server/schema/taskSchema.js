e56a9372b76783787913ba2baf71f4429b8a8249
“joaoviana” <joaopadraoviana@gmail.com>
2019-11-10 19:07:15 +0000

    remove progress field as a require field for a task object

--- server/schema/taskSchema.js
+++ server/schema/taskSchema.js
@@ -1,11 +1,13 @@
 const Joi = require("joi");
 const taskSchema = Joi.object().keys({
-    title: Joi.string().required(), 
+    title: Joi.string().required(),
     description: Joi.string().required(),
-    time: Joi.string().regex(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/).required(),
+    time: Joi.string()
+        .regex(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/)
+        .required(),
     taskHost: Joi.object().keys({
         name: Joi.string().required(),
         title: Joi.string().required()
-    }),
-    progress: Joi.string().regex(/(inprogress|complete)/).required()}); 
+    })
+});
 module.exports = taskSchema;

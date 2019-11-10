c825a4c6b5cf5fe6b9802b0d22bf5607b1981e07
“joaoviana” <joaopadraoviana@gmail.com>
2019-11-10 19:03:58 +0000

    toggle task card progress with axios patch request - clientside

--- client/src/components/TaskCard/TaskCard.js
+++ client/src/components/TaskCard/TaskCard.js
@@ -5,113 +5,102 @@ import Button from "../Button";
 import Badge from "../Badge";
 import ProfileArea from "../ProfileArea";
 import Header from "../Header";
-
-const updateTask = (id, day, toUpdateData = {}, tasksData) => {
-	// TODO update data through API call rather than dummyData
-	/* new obj so state can recognise mutation */
-	let newTasksData = { ...tasksData };
-	let dayTasks = newTasksData[day];
-
-	dayTasks.forEach((task, i) => {
-		if (task.id === id) {
-			newTasksData[day][i] = { ...task, ...toUpdateData };
-			return true;
-		}
-		return false;
-	});
-
-	return newTasksData;
-};
+import axios from "axios";
 
 const TaskCard = ({
-	setTasksData,
-	tasksData,
-	day,
-	id,
-	title,
-	description,
-	time,
-	taskWith,
-	progress,
-	isFiltered = false
+    setTasksData,
+    tasksData,
+    day,
+    id,
+    title,
+    description,
+    time,
+    taskWith,
+    progress,
+    taskId,
+    isFiltered = false
 }) => {
-	// Consts
-	const [taskTypeInprogress, taskTypeComplete] = ["inprogress", "complete"];
-	const [cardTaskType, setCardTaskType] = useState(taskTypeInprogress);
+    // Consts
+    const [taskTypeInprogress, taskTypeComplete] = ["inprogress", "complete"];
+    const [cardTaskType, setCardTaskType] = useState(taskTypeInprogress);
 
-	// conditionals
-	const taskProgressIsInprogress = progress === taskTypeInprogress;
-	const taskProgressIsComplete = progress === taskTypeComplete;
+    // conditionals
+    const taskProgressIsInprogress = progress === taskTypeInprogress;
+    const taskProgressIsComplete = progress === taskTypeComplete;
 
-	useEffect(() => {
-		if (taskProgressIsInprogress || taskProgressIsComplete)
-			setCardTaskType(progress);
-		else setCardTaskType(taskTypeInprogress);
-	}, [
-		progress,
-		setCardTaskType,
-		taskTypeInprogress,
-		taskTypeComplete,
-		taskProgressIsInprogress,
-		taskProgressIsComplete
-	]);
+    useEffect(() => {
+        if (taskProgressIsInprogress || taskProgressIsComplete)
+            setCardTaskType(progress);
+        else setCardTaskType(taskTypeInprogress);
+    }, [
+        progress,
+        setCardTaskType,
+        taskTypeInprogress,
+        taskTypeComplete,
+        taskProgressIsInprogress,
+        taskProgressIsComplete
+    ]);
 
-	// handleClick
-	// TODO update with mongodb call to update data
-	const handleChangeTypeToClick = (newType) => {
-		let newData = { progress: newType };
+    // Conditionals
+    const cardTypeIsInprogress = cardTaskType === taskTypeInprogress;
 
-		setTasksData(updateTask(id, day, newData, tasksData));
-		setCardTaskType(newType);
-	};
+    const btnText = cardTypeIsInprogress ? "Mark as complete" : "Re-open task";
+    const btnCategory = cardTypeIsInprogress ? "primary" : "accent";
 
-	// Conditionals
-	const cardTypeIsInprogress = cardTaskType === taskTypeInprogress;
+    const handleClick = async () => {
+        async function getUsers() {
+            let result = await axios.get("/users");
+            toggleTask(result.data[0]._id);
+        }
+        async function toggleTask(userId) {
+            await axios.patch(`/users/${userId}`, {
+                dayOfTask: day,
+                taskID: taskId
+            });
+            let result = await axios.get(`/users/${userId}`);
 
-	const btnText = cardTypeIsInprogress ? "Mark as complete" : "Re-open task";
-	const btnCategory = cardTypeIsInprogress ? "primary" : "accent";
+            await setTasksData(result.data);
+        }
+        getUsers();
+    };
 
-	const handleClick = cardTypeIsInprogress
-		? () => handleChangeTypeToClick(taskTypeComplete)
-		: () => handleChangeTypeToClick(taskTypeInprogress);
+    const SubHeader = () => (
+        <p className={styles["sub-header"]}>Due by: {time}</p>
+    );
 
-	const SubHeader = () => (
-		<p className={styles["sub-header"]}>Due by: {time}</p>
-	);
-
-	return (
-		<div className={styles.card}>
-			<div
-				className={`${styles["content-wrapper"]} ${
-					styles["content-wrapper__left"]
-				} `}
-			>
-				<div className={styles.header}>
-					<Header text={title} />
-					{<SubHeader />}
-				</div>
-				<div className={styles.description}>
-					<p>{description}</p>
-				</div>
-				<div className={styles["button-wrapper"]}>
-					<Button
-						size={"medium"}
-						buttonText={btnText}
-						onClick={handleClick}
-						category={btnCategory}
-					/>
-				</div>
-			</div>
-			<div
-				className={`${styles["content-wrapper"]} ${
-					styles["content-wrapper__right"]
-				}`}
-			>
-				<Badge type={cardTaskType} />
-				<ProfileArea {...taskWith} />
-			</div>
-		</div>
-	);
+    return (
+        <div className={styles.card}>
+            <div
+                className={`${styles["content-wrapper"]} ${
+                    styles["content-wrapper__left"]
+                } `}
+            >
+                <div className={styles.header}>
+                    <Header text={title} />
+                    {<SubHeader />}
+                </div>
+                <div className={styles.description}>
+                    <p>{description}</p>
+                </div>
+                <div className={styles["button-wrapper"]}>
+                    <Button
+                        size={"medium"}
+                        buttonText={btnText}
+                        onClick={handleClick}
+                        category={btnCategory}
+                    />
+                </div>
+            </div>
+            <div
+                className={`${styles["content-wrapper"]} ${
+                    styles["content-wrapper__right"]
+                }`}
+            >
+                <Badge type={cardTaskType} />
+                <ProfileArea {...taskWith} />
+            </div>
+        </div>
+    );
 };
 
 export default TaskCard;
